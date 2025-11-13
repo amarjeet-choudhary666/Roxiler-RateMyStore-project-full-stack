77e45dc47e4ebe3a774bfdf43dcca7de8c0eea20
Amarjeet choudhary <114072935+amarjeet-choudhary666@users.noreply.github.com>
2025-11-13 18:11:06 +0530

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://roxilor-backend-1.onrender.com/v1/api';
+const API_BASE_URL = 'https://roxilor-backend.onrender.com/v1/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
